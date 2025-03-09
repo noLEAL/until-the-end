9db67bed940df994ac807fa65ba61dd46133aee4
Bruno Leal da Silva Paz <83329725+noLEAL@users.noreply.github.com>
2025-03-09 14:49:48 +0000

    agora vai

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1> Olá, Mundo -- tchau mundo </h1>;
 }
 
-function teste() {
-  console.log("teste teste 123");
-}
-
-function teste() {
-  console.log("TESTANDO Prettier");
-}
-
 export default Home;
