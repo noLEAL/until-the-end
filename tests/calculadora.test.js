c2408956b137f69cb59396255d4a10d8bd185a71
Bruno Leal da Silva Paz <83329725+noLEAL@users.noreply.github.com>
2025-05-01 23:53:05 +0000

    criacão do teste real da calculadora

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,6 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora");
+
+test("somar 2+2 deve ser 4 ", () => {
+  const resultado = calculadora.somar(2, 2);
+  console.log(resultado);
 });
