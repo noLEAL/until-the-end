bbfda4760b5180d20de3e79b3e8207b3217aca75
Bruno Leal da Silva Paz <83329725+noLEAL@users.noreply.github.com>
2025-05-02 00:09:26 +0000

    criado mais de um teste. TDD criar um teste depois desenvolver o codigo

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -2,5 +2,12 @@ const calculadora = require("../models/calculadora");
 
 test("somar 2+2 deve ser 4 ", () => {
   const resultado = calculadora.somar(2, 2);
-  console.log(resultado);
+  expect(resultado).toBe(4);
+  // console.log(resultado);
+});
+
+test("somar 5 + 100 deve ser 105 ", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+  // console.log(resultado);
 });
