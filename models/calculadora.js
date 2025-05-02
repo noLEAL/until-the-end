c1bf055e4e9b098011326072fef7ac51c121cc61
Bruno Leal da Silva Paz <83329725+noLEAL@users.noreply.github.com>
2025-05-02 02:04:20 +0000

    adiciona testes para as funções de soma, subtração e divisão na calculadora

--- models/calculadora.js
+++ models/calculadora.js
@@ -1,5 +1,33 @@
 function somar(numero1, numero2) {
+  if (typeof numero1 !== "number") {
+    return "Erro";
+  }
   return numero1 + numero2;
 }
 
 exports.somar = somar;
+
+// = - igual atribuição, serve para atribuir valor a alguma coisa
+// == - comparação fraca, compara dois valores apenas quanto ao valor, ignorando o tipo
+//    10 == '10'; // true — compara o valor, ignora o tipo (número vs string)
+//=== - comparação forte, compara considerando o tipo e o valor
+//     10 === '10'; // false — tipos diferentes (number !== string)
+
+function subtrair(numero1, numero2) {
+  if (numero1 == null) {
+    return "errrrro";
+  }
+  return numero1 - numero2;
+}
+
+exports.subtrair = subtrair;
+
+function dividir(numero1, numero2) {
+  if (numero2 === 0) {
+    var resultadoDiv = 50 / 0;
+    return resultadoDiv;
+  }
+  return numero1 / numero2;
+}
+
+exports.dividir = dividir;
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -11,3 +11,23 @@ test("somar 5 + 100 deve ser 105 ", () => {
   expect(resultado).toBe(105);
   // console.log(resultado);
 });
+
+test("somar 'banana' + 100 deve ser erro ", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
+  // console.log(resultado);
+});
+
+test("subtrair 'null' - 50 deve ser erro", () => {
+  const resultado = calculadora.subtrair(null, 50);
+  expect(resultado).toBe("errrrro");
+  // console.log(resultado);
+});
+
+test("dividir numero1 / 0 deve ser um Infinity do tipo 'numero' ", () => {
+  const resultado = calculadora.dividir(10, 0);
+  console.log(resultado);
+  console.log(typeof resultado);
+  expect(resultado).toBe(Infinity);
+  expect(typeof resultado).toBe("number");
+});
